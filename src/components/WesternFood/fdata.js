27a4caa375360130def36e9203e38d39523a058e
Greatbok <simso108@gmail.com>
2023-01-04 19:03:21 +0900

    western1

---
+++ src/components/WesternFood/fdata.js
@@ -0,0 +1,31 @@
+let fdata = [
+  {
+    id: 0,
+    title: "라따뚜이",
+    price: 17000,
+    image:
+      "https://blog.kakaocdn.net/dn/ce377Z/btrfa2jsnT4/75D0sJ0weOGUMwPmtj86Mk/img.jpg",
+  },
+  {
+    id: 1,
+    title: "뵈프부르기뇽",
+    price: 22000,
+    image:
+      "https://image.idus.com/image/files/8a917bdfb749459aabb67772d81bb8b0_512.jpg",
+  },
+  {
+    id: 2,
+    title: "라구소스파스타",
+    price: 17000,
+    image: "https://img.siksinhot.com/seeon/1606792502279350.jpg",
+  },
+  {
+    id: 3,
+    title: "시금치크림파스타",
+    price: 17000,
+    image:
+      "https://blog.kakaocdn.net/dn/Ekajd/btre7Jq9n25/eiJtIb9loP4x11pwrRwgDk/img.jpg",
+  },
+];
+
+export default fdata;
